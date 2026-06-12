73ca6ca32ffa5f2e63241010ea47ec6809a4d26d
Gustavo Favini <gfavini@gmail.com>
2026-06-12 08:40:35 -0300

    Limpesa

--- src/main/java/br/com/portal_nr1/application/ports/in/CreateBookUseCase.java
+++
@@ -1,7 +0,0 @@
-package br.com.portal_nr1.application.ports.in;
-
-import br.com.portal_nr1.domain.model.Book;
-
-public interface CreateBookUseCase {
-	Book createBook(String title, String author);
-}
--- src/main/java/br/com/portal_nr1/application/ports/out/BookRepositoryPort.java
+++
@@ -1,7 +0,0 @@
-package br.com.portal_nr1.application.ports.out;
-
-import br.com.portal_nr1.domain.model.Book;
-
-public interface BookRepositoryPort {
-	Book save(Book book);
-}
